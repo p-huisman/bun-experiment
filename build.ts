42017dd433ccd5cb6f805c2eac3fddb23c5ee2ec
Peter Huisman <138681831+p-huisman@users.noreply.github.com>
2023-09-13 08:39:30 +0200

    dev server

--- build.ts
+++ build.ts
@@ -1,20 +1,85 @@
-import Run from 'bun';
-
 import type { BunPlugin } from "bun";
 
-const myPlugin: BunPlugin = {
-  name: "Custom loader",
-  setup(build) {
-    build.onLoad({filter: /\.css$/}, async (args) => {
-        const contents = await Bun.file(args.path).text();
-        return {loader: "text", contents};
-      });
+import Run from "bun";
+import { watch } from "fs";
+import postcss from "postcss";
+
+const postcssPlugin: BunPlugin = {
+  name: "Postcss loader",
+  setup(build: Run.PluginBuilder) {
+    build.onLoad({ filter: /\.css$/ }, async (args) => {
+      let contents = await Bun.file(args.path).text();
+      contents = await postcss([
+        require("postcss-preset-env")({
+          grid: true,
+        }),
+      ]).process(contents, { from: undefined }).css;
+      return { loader: "text", contents };
+    });
   },
 };
 
-Run.build({
-    entrypoints: ['./dummy-component.tsx'],
-    outdir: './dist',
-    format: 'esm',
-    plugins: [myPlugin],
+const connnectedSockets: Run.ServerWebSocket<unknown>[] = [];
+
+const build = () => {
+  Run.build({
+    entrypoints: ["./src/dummy-component.tsx"],
+    outdir: "./dist",
+    format: "esm",
+    minify: true,
+    sourcemap: "external",
+    plugins: [postcssPlugin],
+  })
+    .then(() => console.log("build"))
+    .catch((err) => console.error(err));
+};
+build();
+
+const watcher = watch("./src", () => {
+  build();
+  connnectedSockets.forEach((ws) => ws.send("reload"));
 });
+
+process.on("SIGINT", () => {
+  console.log(" closing...");
+  watcher.close();
+  connnectedSockets.forEach((ws) => ws.send("close"));
+  process.exit(0);
+});
+Bun.serve(
+  { 
+    port: 9090, 
+    fetch: async (req, server) => {
+      const success = server.upgrade(req);
+      if (success) {
+        console.log("upgrade");
+        return undefined;
+      }
+      const  { pathname } = new URL(req.url);
+      const f = Bun.file(`.${pathname}`);
+      if (f) {
+        const r = new Response(await f.arrayBuffer().catch(e => e));
+        if (r instanceof Response) {
+          return new Response(f);
+        }
+      }
+      return new Response("Not found", { status: 404 });
+    },
+    error(e) {
+      if(e.message === "No such file or directory"){
+        return new Response("Not found", { status: 404 });
+      }
+      return new Response("Error§", { status: 500 });
+    },
+    websocket: {
+      message() {
+      },
+      open(ws) {
+        connnectedSockets.push(ws);
+      }, 
+      close(ws) {
+        connnectedSockets.splice(connnectedSockets.indexOf(ws), 1);
+      }
+    },
+  }
+);
